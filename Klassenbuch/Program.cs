a07872a1b0af3f25e16932e7e5b9559e277e0f8d
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-09 09:56:28 +0200

    cleaned up Form1.cs

--- Klassenbuch/Program.cs
+++ Klassenbuch/Program.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Klassenbuch
 {
     static class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
         [STAThread]
         static void Main()
         {
